2d418d78e9678f04311bbef714960a6f18f09add
hlaolil <hla24002@byui.edu>
2026-01-21 20:45:07 +0200

    w2

--- week02/teach/CustomerService.cs
+++ week02/teach/CustomerService.cs
@@ -10,25 +10,54 @@ public class CustomerService {
 
         // Test Cases
 
-        // Test 1
-        // Scenario: 
-        // Expected Result: 
-        Console.WriteLine("Test 1");
+        // Test 1: Size of Queue
+        // Scenario: input invalid size (0 or negative)
+        // Expected Result: must defualt to 10
+        var cs1 = new CustomerService(-5);
+        Console.WriteLine(cs1);
 
         // Defect(s) Found: 
 
         Console.WriteLine("=================");
 
-        // Test 2
-        // Scenario: 
-        // Expected Result: 
-        Console.WriteLine("Test 2");
+        // Test 2: Add New Customer
+        // Scenario: Add customers until queue is full, then try to add one more
+        // Expected Result: After adding maxSize customers → shows "Maximum Number of Customers in Queue."
+        var cs3 = new CustomerService(3);
+        
+        cs3.AddNewCustomer(); // 1
+        cs3.AddNewCustomer(); // 2
+        cs3.AddNewCustomer(); // 3
+        Console.WriteLine(cs3); // size should be 3
 
-        // Defect(s) Found: 
+        cs3.AddNewCustomer(); // should show error message
+        Console.WriteLine(cs3); // size should still be 3
+
+        // Defect(s) Found: continues to add customers beyond max size
 
         Console.WriteLine("=================");
 
         // Add more Test Cases As Needed Below
+        //Test 3: Serve Customer
+        // Scenario: Add customers and serve them one by one
+        // Expected Result: Customers are served in the order they were added (FIFO)
+        var cs4 = new CustomerService(3);
+        cs4.AddNewCustomer(); // 1
+        cs4.AddNewCustomer(); // 2
+        cs4.AddNewCustomer(); // 3
+        Console.WriteLine(cs4); // size should be 3
+        cs4.ServeCustomer(); // serves 1
+        Console.WriteLine(cs4); // size should be 2
+
+        cs4.ServeCustomer(); // serves 2
+        cs4.ServeCustomer(); // serves 3
+        Console.WriteLine(cs4); // size should be 0
+
+        cs4.ServeCustomer(); // should show error message
+        // Defect(s) Found: does not handle serving from an empty queue
+
+        Console.WriteLine(cs4);
+
     }
 
     private readonly List<Customer> _queue = new();
